6434f8136e3306ed15fe4883e76fca6b0f112289
Colin Chan <colinchan@lumeh.org>
2014-11-18 19:58:51 -0500

    Add form for adding levels

--- floorpaint-server.py
+++ floorpaint-server.py
@@ -14,7 +14,9 @@ from flask import abort
 from flask import Flask
 from flask import g
 from flask import redirect
+from flask import Response
 from flask import request
+from flask import send_file
 from flask import session
 from flask import url_for
 
@@ -43,9 +45,9 @@ def teardown_request(exception):
 def main_page():
     return "Hi!"
 
-@app.route('/level/<id>')
-def get_level(id):
-    cur = g.db.execute('SELECT * FROM levels WHERE id=?', id)
+@app.route('/success')
+def success():
+    return "Success!"
 
 @app.route('/level/random')
 def get_random_level():
@@ -58,21 +60,27 @@ def get_random_level():
     cur = g.db.execute(query)
     levels = cur.fetchall()
     level = random.choice(levels)
-    return flask.Response(level[0], mimetype='text/plain')
+    return Response(level[0], mimetype='text/plain')
+
+@app.route('/level/new')
+def new_level():
+    return send_file(open(os.path.join(app.root_path, 'static/new.html'), 'rb'),
+                     mimetype='text/html')
 
 @app.route('/level', methods=['POST'])
-def add_entry():
+def post_level():
     level = (request.form['id'],
              request.form['difficulty'],
-             False)
+             True)
     g.db.execute('INSERT INTO levels VALUES (?, ?, ?)', level)
     g.db.commit()
-    return redirect(url_for('get_level', request.form['id']))
+    return redirect(url_for('success'))
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__)
-    app.config['DEBUG'] = arguments['--debug']
-    app.config['DATABASE'] = os.path.join(app.root_path, arguments['DATABASE_FILE'])
+    args = docopt(__doc__)
+    app.config['DEBUG'] = args['--debug']
+    app.config['DATABASE'] = os.path.join(app.root_path,
+                                          args['DATABASE_FILE'])
     if not os.path.exists(app.config['DATABASE']):
         init_db()
     app.run()
